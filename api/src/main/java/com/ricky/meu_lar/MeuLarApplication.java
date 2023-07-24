1f784329dddd9751cffae24ff05012f5f3d4549e
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2023-07-24 08:34:14 -0300

    projeto vazio spring

---
+++ api/src/main/java/com/ricky/meu_lar/MeuLarApplication.java
@@ -0,0 +1,13 @@
+package com.ricky.meu_lar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuLarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuLarApplication.class, args);
+	}
+
+}
